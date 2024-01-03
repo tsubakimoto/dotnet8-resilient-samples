4777585ddda685718f50aed08b3b52d5c3655697
Yuta Matsumura <tkmbmbr@gmail.com>
2024-01-03 22:00:10 +0900

    update code

--- ResilientWebApiApp/Program.cs
+++ ResilientWebApiApp/Program.cs
@@ -1,8 +1,6 @@
-using Microsoft.Extensions.DependencyInjection;
-// using Microsoft.Extensions.Http.Polly;
-using Microsoft.Extensions.Http.Resilience;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Resilience;
+using Polly;
+using Polly.Timeout;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,23 +10,22 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddHttpClient("my-client")
-        .AddResilienceHandler("my-pipeline", builder =>
-        {
-            // Refer to https://www.pollydocs.org/strategies/retry.html#defaults for retry defaults
-            builder.AddRetry(new HttpRetryStrategyOptions
-            {
-                MaxRetryAttempts = 4,
-                Delay = TimeSpan.FromSeconds(2),
-                BackoffType = DelayBackoffType.Exponential
-            });
+                .AddStandardResilienceHandler(options =>
+                {
+                    // Customize retry
+                    options.Retry.ShouldHandle = new PredicateBuilder<HttpResponseMessage>()
+                        .Handle<TimeoutRejectedException>()
+                        .Handle<HttpRequestException>()
+                        .HandleResult(response => response.StatusCode == HttpStatusCode.InternalServerError);
+                    options.Retry.MaxRetryAttempts = 5;
 
-            // Refer to https://www.pollydocs.org/strategies/timeout.html#defaults for timeout defaults
-            builder.AddTimeout(TimeSpan.FromSeconds(5));
-        });
-        // .AddStandardResilienceHandler(options =>
-        // {
-        //     // Configure standard resilience options here
-        // });
+                    // Customize attempt timeout
+                    options.AttemptTimeout.Timeout = TimeSpan.FromSeconds(2);
+                });
+                //.AddStandardResilienceHandler(options =>
+                //{
+                //    // Configure standard resilience options here
+                //});
 
 var app = builder.Build();
 
@@ -65,6 +62,7 @@ app.MapGet("/echo", async (IHttpClientFactory httpClientFactory) =>
 {
     var httpClient = httpClientFactory.CreateClient("my-client");
     var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/comments");
+    //var response = await httpClient.GetAsync("https://www.microsoft.dev/");
     return response;
 })
 .WithName("Echo")
